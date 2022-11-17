abae7e881594de823f825fafe070c450489cd713
lucascr91 <lucas.ecomg@gmail.com>
2022-11-17 15:22:27 -0300

    [infra] add basedosdados files

--- basedosdados/script.py
+++ basedosdados/script.py
@@ -1,59 +1,109 @@
-from google.cloud import storage
+# pylint: disable=invalid-name,line-too-long
+import re
+import os
+from glob import glob
+import sys
+
 import pandas as pd
-from basedosdados import read_sql
+from unidecode import unidecode
+from tqdm import tqdm
+
+from utils import create_table_and_upload_to_gcs
 
 
-def get_users_dict():
-    """ Get users from Google Cloud Storage """
+def get_caged_data(table_id: str, yearmonth: int) -> None:
+    """
+    Get CAGED data
 
-    client = storage.Client()
-    bucket = client.get_bucket('basedosdados-dev')
-    blobs = list(bucket.list_blobs(prefix='staging/br_bd_indicadores/website_user/'))
-    map_blobs = {}
-    for blob in blobs:
-        name = blob.name
-        date = name.split('/')[3].split('=')[1]
-        map_blobs[date] = blob.public_url
+    table_id: microdados_movimentacao | microdados_movimentacao_fora_prazo | microdados_movimentacao_excluida
+    yearmonth: format YYYYMM
+    """
+    year = str(yearmonth)[:4]
+    if int(year) not in [2020, 2021, 2022]:
+        raise ValueError("Year must be 2020, 2021 or 2022")
+    groups = {
+        "microdados_movimentacao": "cagedmov",
+        "microdados_movimentacao_fora_prazo": "cagedfor",
+        "microdados_movimentacao_excluida": "cageddex",
+    }
 
-    # get list of ordered dates
-    dates = list(map_blobs.keys())
-    dates.sort()
+    group = groups[table_id]
+    command = f"bash download.sh {group} {table_id} {yearmonth}"
 
-    df = pd.read_csv(map_blobs[dates[-1]])
+    os.system(command)
 
-    users = df.to_dict(orient='records')[0]
 
-    return users
+def build_partitions(table_id: str) -> str:
+    """
+    build partitions from gtup files
+    table_id: microdados_movimentacao | microdados_movimentacao_fora_prazo | microdados_movimentacao_excluida
+    """
+    input_files = glob(f"/tmp/caged/{table_id}/input/*txt")
+    for filename in tqdm(input_files):
+        df = pd.read_csv(filename, sep=";", dtype={"uf": str})
+        date = re.search(r"\d+", filename).group()
+        ano = date[:4]
+        mes = int(date[-2:])
 
-def get_consulta_dict():
-    """ Get access statistics from Big Query """
+        df.columns = [unidecode(col) for col in df.columns]
 
-    query = '''
-        WITH dados AS(
-        SELECT timestamp_trunc(`basedosdados.br_bd_indicadores.acessos_tabelas_bigquery`.`estampa_tempo`, month) AS `estampa_tempo`, count(*) AS `count`
-        FROM `basedosdados.br_bd_indicadores.acessos_tabelas_bigquery`
-        WHERE ((`basedosdados.br_bd_indicadores.acessos_tabelas_bigquery`.`tipo_usuario` <> 'admin'
-            OR `basedosdados.br_bd_indicadores.acessos_tabelas_bigquery`.`tipo_usuario` IS NULL)
-            AND `basedosdados.br_bd_indicadores.acessos_tabelas_bigquery`.`estampa_tempo` >= timestamp "2022-01-01 00:00:00 America/Sao_Paulo")
-        GROUP BY `estampa_tempo`
-        ORDER BY `estampa_tempo` ASC)
+        dict_uf = {
+            "11": "RO",
+            "12": "AC",
+            "13": "AM",
+            "14": "RR",
+            "15": "PA",
+            "16": "AP",
+            "17": "TO",
+            "21": "MA",
+            "22": "PI",
+            "23": "CE",
+            "24": "RN",
+            "25": "PB",
+            "26": "PE",
+            "27": "AL",
+            "28": "SE",
+            "29": "BA",
+            "31": "MG",
+            "32": "ES",
+            "33": "RJ",
+            "35": "SP",
+            "41": "PR",
+            "42": "SC",
+            "43": "RS",
+            "50": "MS",
+            "51": "MT",
+            "52": "GO",
+            "53": "DF",
+        }
 
-        SELECT DATE(estampa_tempo) AS month, count
-        From dados
-    '''
-    df = read_sql(query=query, billing_project_id='basedosdados-dev', from_file=True)
-    # convert month to string
-    df['month'] = df['month'].astype(str)
-    result = df.set_index('month').to_dict(orient='index')
+        df["uf"] = df["uf"].map(dict_uf)
 
-    return result
+        for state in dict_uf.values():
+            data = df[df["uf"] == state]
+            data.drop(["competenciamov", "uf"], axis=1, inplace=True)
+            data.to_csv(
+                f"/tmp/caged/{table_id}/ano={ano}/mes={mes}/sigla_uf={state}/data.csv",
+                index=False,
+            )
+            del data
+        del df
+
+    return f"/tmp/caged/{table_id}/"
+
 
 def main():
-    consulta = get_consulta_dict()
-    users = get_users_dict()
+    table_id = sys.argv[1]
+    dataset_id = "br_me_caged"
+    yearmonth = int(sys.argv[2])
+    get_caged_data(table_id, yearmonth)
+    filepath = build_partitions(table_id)
+    create_table_and_upload_to_gcs(
+            data_path=filepath,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            dump_mode="append",
+        )
 
-    print({'consulta': consulta, 'website_users': users})
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
